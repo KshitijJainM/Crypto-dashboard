eaf2b08522ddb5d1e0949df5c1e552b92cae20ff
KshitijJainM <2018ise_kshitijjainm@nie.ac.in>
2021-06-30 20:58:19 +0530

    Project Complete

---
+++ components/Layout.js
@@ -0,0 +1,33 @@
+import Head from "next/head";
+import Link from "next/link";
+
+const Layout = ({ children, title = "Crypto Tracker" }) => {
+    return ( <
+        div className = "layout" >
+        <
+        Head >
+        <
+        title > { title } < /title> <
+        link rel = "icon"
+        href = "/favicon.ico" / >
+        <
+        /Head> <
+        header className = "header" >
+        <
+        Link href = "/"
+        passHref >
+        <
+        a >
+        <
+        img src = "ct.png"
+        alt = "" / >
+        <
+        /a> <
+        /Link> <
+        /header> <
+        main > { children } < /main> <
+        /div>
+    );
+};
+
+export default Layout;
